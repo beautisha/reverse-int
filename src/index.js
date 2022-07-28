94e4ac0d07e377eeb3cd4457a0e4ec23dc944e7e
beautisha <korobochky@yandex.ru>
2022-07-28 12:54:18 +0300

    solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let arr = Array.from(String(n));
+  if (arr[0] == '-') {
+    arr.splice(0,1);
+  }
+  if (arr[arr.length-1] == 0) {
+    arr.splice(arr.length-1, 1);
+  }
+  arr = arr.reverse();
+  return (Number(arr.toString().replaceAll(',','')));
 }
